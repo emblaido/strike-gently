19ce0c4394756a40e365302f4c8eb457f15377d4
Emma Blaido <emmablaido@Emmas-Air.lan>
2023-01-30 15:47:07 -0600

    having trouble with views.py

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Art
 # Create your views here.
 
 
@@ -18,10 +19,20 @@ class Art:
         self.image = image
         self.bio = bio
 
+        art = [
+  Art("Gorillaz", "https://i.scdn.co/image/ab67616d00001e0295cf976d9ab7320469a00a29",
+          "Gorillaz are once again disrupting the paradigm and breaking convention in their round the back door fashion with Song Machine, the newest concept from one of the most inventive bands around."),]
+ 
+
 class ArtList(TemplateView):
     template_name = "Art_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["Art"] = Art
+        name = self.request.GET.get("name")
+        if name != None:
+            context["art"] = Art.objects.filter(name__icontains=name)
+        else:
+            context["art"] = Art.objects.all()
+            context["header"] = "Art Therapy"
         return context
