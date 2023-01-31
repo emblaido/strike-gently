8ee7b82615bc3ef59b0378f09dd20fda3e91b2b2
Emma Blaido <emmablaido@Emmas-Air.lan>
2023-01-31 09:44:56 -0600

    added imports

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
-        path('art/', views.ArtList.as_view(), name="art_list")
+    path('art/', views.ArtList.as_view(), name="art_list"),
+    path('artists/new/', views.ArtCreate.as_view(), name="art_create")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
+#from django.views import View
+#from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from .models import Art
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 
@@ -13,15 +14,15 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-class Art:
-    def __init__(self, name, image, bio):
-        self.name = name
-        self.image = image
-        self.bio = bio
+#class Art:
+    #def __init__(self, name, image, bio):
+       # self.name = name
+        #self.image = image
+       # self.bio = bio
 
-        art = [
-  Art("Gorillaz", "https://i.scdn.co/image/ab67616d00001e0295cf976d9ab7320469a00a29",
-          "Gorillaz are once again disrupting the paradigm and breaking convention in their round the back door fashion with Song Machine, the newest concept from one of the most inventive bands around."),]
+        #art = [
+ # Art("Gorillaz", "https://i.scdn.co/image/ab67616d00001e0295cf976d9ab7320469a00a29",
+          #"Gorillaz are once again disrupting the paradigm and breaking convention in their round the back door fashion with Song Machine, the newest concept from one of the most inventive bands around."),]
  
 
 class ArtList(TemplateView):
@@ -34,5 +35,11 @@ class ArtList(TemplateView):
             context["art"] = Art.objects.filter(name__icontains=name)
         else:
             context["art"] = Art.objects.all()
-            context["header"] = "Art Therapy"
+            context["header"] = "Art"
         return context
+
+class ArtCreate(CreateView):
+    model = Art
+    fields = ['name', 'img', 'bio',]
+    template_name = "art_create.html"
+    success_url = "/art/"
