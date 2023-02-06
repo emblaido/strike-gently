66eb6994547535ac94ffe1e0c744cd874d8bf018
Emma Blaido <emmablaido@Emmas-Air.lan>
2023-02-06 01:04:19 -0600

    final touches

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # strike-gently
+
+https://strike-gently.herokuapp.com/
+
+A app by Emma Blaido in Django and python
+
+Our app is designed to be a gentle approach for those who are struggling or are prone to mental health issues, while we are not a replacement for proper medical treatment, we do believe that steps can be taken to prevent or lessen the effects of depression and anxiety.
+
+studies have shown that through proper sleep, diet, and exercise one has a greater chance of preventing relapses, and through small self-care acts one can naturally produce serotonin to help in deep depressive episodes.
+
+the user can select from several informational resources or therapies to learn more about daily habits that could be affecting their mental health or small steps they could take to improve their state of mind 
+
+the user may also create an account to log triggers, mood swings, and general feelings either in depressive episodes or continuously journal as an act of self-care, keeping a record of the highs and lows is a wonderful way to deeper understand what one may be struggling with, with understanding comes to the power to identify more effective forms of treatment, "what works best for me?" "what triggered me so hard last time?" "how did I come out of it?" "how long did it last?" being able to answer these questions will help our users in the long run which is why we have a search feature readily available to find old journals.
+
+
+For this project I used several GA-provided notations on Django and python functions, I also received assistance from my Pod leader and the office hours instructor (thank you to you both) 
+
+technologies used were Django, Python, SQL, 
+
+to run my app, please fork and clone my repo, and install the necessary technologies, 
+
+in your terminal
+pip3 install pipenv 
+pipenv install django 
+pipenv shell
+python3 manage.py runserver
+
+future features include 
+• Daily reminders 
+• dates posted 
+•more in-depth informaional pages 
+• therapist talk lines 
+•pages for outreach programs 
+
+
+bugs- 
+•infromation stopped displaying for therapies
--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
@@ -36,22 +36,23 @@ class TherapyList(TemplateView):
         context["therapy"] = Therapy.objects.all()
         return context
 
+
 class TherapyDetail(DetailView):
-     model = Therapy
-     template_name = "therapy_detail.html"
-    
-   
+    model = Therapy
+    template_name = "therapy_detail.html"
+
+
 @method_decorator(login_required, name='dispatch')
 class JournalList(TemplateView):
     template_name = "journal_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #name = self.request.GET.get("name")
+        # name = self.request.GET.get("name")
         user = self.request.user
         print('user')
         print(self.request.user)
-        if user != None: 
+        if user != None:
             context["journal"] = Journal.objects.all()
             context['cur_user'] = user
         else:
@@ -60,19 +61,24 @@ class JournalList(TemplateView):
             context["header"] = "journal"
         return context
 
+
 class JournalCreate(CreateView):
     model = Journal
     fields = ['name', 'img', 'bio',]
     template_name = "journal_create.html"
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(JournalCreate, self).form_valid(form)
+
     def get_success_url(self):
         return reverse('journal_detail', kwargs={'pk': self.object.pk})
+
 
 class JournalDetail(DetailView):
     model = Journal
     template_name = "journal_detail.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["journals"] = Journal.objects.all()
@@ -80,24 +86,29 @@ class JournalDetail(DetailView):
         context = super().get_context_data(**kwargs)
         return context
 
+
 class JournalUpdate(UpdateView):
     model = Journal
     fields = ['name', 'img', 'bio',]
     template_name = "journal_update.html"
     success_url = "/journal/"
+
     def get_success_url(self):
         return reverse('journal_detail', kwargs={'pk': self.object.pk})
+
 
 class JournalDelete(DeleteView):
     model = Journal
     template_name = "journal_delete_confirmation.html"
     success_url = "/journal/"
 
+
 class Signup(View):
     def get(self, request):
         form = UserCreationForm()
         context = {"form": form}
         return render(request, "registration/signup.html", context)
+
     def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -107,4 +118,3 @@ class Signup(View):
         else:
             context = {"form": form}
             return render(request, "registration/signup.html", context)
-
